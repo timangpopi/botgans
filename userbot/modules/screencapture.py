6067dc9e483f6c68f921d05476dc22aef1b62dc5
Ajuandp <49325037+andreasjp0@users.noreply.github.com>
2019-08-12 20:42:05 +0700

    Update screencapture.py

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -15,8 +15,7 @@ from userbot.events import register, errors_handler
 from userbot import GOOGLE_CHROME_BIN, CHROME_DRIVER, CMD_HELP
 
 
-@register(pattern=r".screencapture (.*)", outgoing=True)
-@errors_handler
+@register(pattern=r".scrn (.*)", outgoing=True)
 async def capture(url):
     """ For .screencapture command, capture a website and send the photo. """
     if not url.text[0].isalpha() and url.text[0] not in ("/", "#", "@", "!"):
