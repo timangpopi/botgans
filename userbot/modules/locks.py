9c6c14b1744e37dd1d60a7bd0b9fe070123b734c
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-08-24 21:19:17 +0530

    added unlock module !!

--- userbot/modules/locks.py
+++ userbot/modules/locks.py
@@ -67,10 +67,71 @@ async def locks(event):
             await sleep(5)
             await event.delete()
 
+
+@register(outgoing=True, pattern=r"^.unlock ?(.*)")
+@errors_handler
+async def rem_locks(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        input_str = event.pattern_match.group(1)
+        peer_id = event.chat_id
+        msg = None
+        media = None
+        sticker = None
+        gif = None
+        gamee = None
+        ainline = None
+        gpoll = None
+        adduser = None
+        cpin = None
+        changeinfo = None
+        if "msg" in input_str:
+            msg = False
+        if "media" in input_str:
+            media = False
+        if "sticker" in input_str:
+            sticker = False
+        if "gif" in input_str:
+            gif = False
+        if "game" in input_str:
+            gamee = False
+        if "inline" in input_str:
+            ainline = False
+        if "poll" in input_str:
+            gpoll = False
+        if "invite" in input_str:
+            adduser = False
+        if "pin" in input_str:
+            cpin = False
+        if "info" in input_str:
+            changeinfo = False
+        banned_rights=ChatBannedRights(
+            until_date=None,
+            send_messages=msg,
+            send_media=media,
+            send_stickers=sticker,
+            send_gifs=gif,
+            send_games=gamee,
+            send_inline=ainline,
+            send_polls=gpoll,
+            invite_users=adduser,
+            pin_messages=cpin,
+            change_info=changeinfo,
+        )
+        try:
+            result = await event.client(EditChatDefaultBannedRightsRequest(
+                peer=peer_id,
+                banned_rights=banned_rights
+            ))
+        except Exception as e:
+            await event.edit(str(e))
+        else:
+            await sleep(5)
+            await event.delete()
+
 CMD_HELP.update({
-    "locks": ".lock <type(s)>\
-\nUsage: Allows you to lock away some common message types in the chat.\
-\n\nNOTE: Requires admin rights in the chat !!\
-\n\nAvailable message types to lock are: \
+    "locks": ".lock <type(s)> or .unlock <type(s)>\
+\nUsage: Allows you to lock/unlock some common message types in the chat.\
+[NOTE: Requires proper admin rights in the chat !!]\
+\n\nAvailable message types to lock/unlock are: \
 \n`msg, media, sticker, gif, game, inline, poll, invite, pin, info`\
 "})
